48425f869a852b765b283e2e764abe1edb1a3c82
RUAN RICKELME LUCENA RAMOS <ruanlucenaramos@gmail.com>
2024-08-11 09:54:03 -0300

    setting up the bot config again

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,31 @@
-import { create } from "venom-bot";
-import { app, port } from "./index";
-import { initializeRoutes } from "./routes/Messages";
+import { Request, Response } from "express";
+import venom from "venom-bot";
+import { app } from "./index";
+import { port } from "./index";
 
-create({
-  session: "zap",
-})
-  .then((client) => {
-    initializeRoutes(client);
-    app.listen(port, () => {
-      console.log(`Server running on port ${port}`);
-    });
-  })
-  .catch((erro) => {
-    console.log(erro);
+export async function start(client: venom.Whatsapp) {
+  app.post("/send", (req: Request, res: Response) => {
+    const { number, message } = req.body;
+
+    if (!number || !message) {
+      return res
+        .status(400)
+        .json({ message: "Número e mensagem são obrigatórios" });
+    }
+
+    client
+      .sendText(`${number}@c.us`, message)
+      .then((result) => {
+        res.status(200).json({ message: "Mensagem enviada com sucesso" });
+      })
+      .catch((erro) => {
+        res
+          .status(500)
+          .json({ message: "Erro ao enviar mensagem", error: erro });
+      });
   });
+
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
+}
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,15 @@
 import express from "express";
+import { create } from "venom-bot";
+import { start } from "./app";
+
 export const app = express();
 export const port = 2222;
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+create({
+  session: "zap",
+})
+  .then((client) => start(client))
+  .catch((erro) => {
+    console.error(erro);
+  });
